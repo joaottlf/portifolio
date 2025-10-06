ab66992f77d8674ab44ea7ee58add7f0de008cb5
João Augustto Lima <850015@senacminas.edu.br>
2025-10-06 17:27:24 -0300

    Iniciar estrutura da tela inicial

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -3,17 +3,39 @@ document.addEventListener('DOMContentLoaded', () => {
 /*  ==========================================================================
     Animação de Introdução
     ========================================================================== */
-    const introOverlay = document.querySelector('.intro-overlay');
-    const leftPanel = document.querySelector('.intro-panel.left');
-    const rightPanel = document.querySelector('.intro-panel.right');
+    const intro_overlay = document.querySelector('.intro-overlay');
+    const left_panel = document.querySelector('.intro-panel.left');
+    const right_panel = document.querySelector('.intro-panel.right');
 
     setTimeout(() => {
-        leftPanel.classList.add('animate-out');
-        rightPanel.classList.add('animate-out');
+        left_panel.classList.add('animate-out');
+        right_panel.classList.add('animate-out');
     }, 500);
 
-    leftPanel.addEventListener('transitionend', () => {
-        introOverlay.remove();
+    left_panel.addEventListener('transitionend', () => {
+        intro_overlay.remove();
     });
 
+/*  ==========================================================================
+    Navegação do menu
+    ========================================================================== */    
+    const links = document.querySelectorAll('.nav_item');
+    const pages = document.querySelectorAll('.page');
+
+    links.forEach( link => {
+        link.addEventListener("click", function(event) {
+            event.preventDefault();
+
+            const targed_id = link.getAttribute('href');
+
+            pages.forEach(page => {
+                page.classList.remove('active');
+            });
+
+            const targed_page = document.querySelector(targed_id);
+            if(targed_page) {
+                targed_page.classList.add('active');
+            }
+        })
+    })
 });
